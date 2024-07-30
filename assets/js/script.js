41367a74e44a3ce02a19d73b9a7d0107a96ee12d
Amila Beganovic <amila.beganovic97@gmail.com>
2024-07-30 08:56:38 +0000

    Write endQuiz function and update Event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,58 @@
 const questions = [{
-    question: "Where is the Colosseum?",
-    options: ["Berlin", "Madrid", "Paris", "Rome"],
-    answer: "Rome"
-},
-{
-    question: "Which country is in Europa?",
-    options: ["Florida", "Peru", "Bosnia and Herzegowina", "India"],
-    answer: "Bosnia and Herzegowina"
-},
-{
-    question: "What is the Capital of Macedonia?",
-    options: ["Skopje", "Sarajevo", "Zagreb"],
-    answer: "Skopje"
-},
-{
-    question: "On which continents is Istanbul located?",
-    options: ["Europa and Asia", "Asia and Africa", "South and North America"],
-    answer: "Europa and Asia"
-},
-{
-    question: "Which  river flows through Egypt?",
-    options: ["Amazon", "Nile", "Una"],
-    answer: "Nile"
-},
-{
-    question: "Which country belong to the Scandinavian countries?",
-    options: ["England", "Sweden", "Montenegro", "Germany", "Poland"],
-    answer: "Sweden"
-},
-{
-    question: "Where is Disneyland in Europa?",
-    options: ["Munich", "Paris", "Vienna"],
-    answer: "Paris"
-},
-{
-    question: "How many continents are there in the World?",
-    options: ["5", "7", "6"],
-    answer: "7"
-},
-{
-    question: "What is the smallest country in the World?",
-    options: ["Monaco", "Vatican City", "Nauru"],
-    answer: "Vatican City"
-},
-{
-    question: "What is the largest desert in the world?",
-    options: ["Sahara", "Antartica", "Gobi"],
-    answer: "Antartica"
-},
+        question: "Where is the Colosseum?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: "Rome"
+    },
+    {
+        question: "Which country is in Europa?",
+        options: ["Florida", "Peru", "Bosnia and Herzegowina", "India"],
+        answer: "Bosnia and Herzegowina"
+    },
+    {
+        question: "What is the Capital of Macedonia?",
+        options: ["Skopje", "Sarajevo", "Zagreb"],
+        answer: "Skopje"
+    },
+    {
+        question: "On which continents is Istanbul located?",
+        options: ["Europa and Asia", "Asia and Africa", "South and North America"],
+        answer: "Europa and Asia"
+    },
+    {
+        question: "Which  river flows through Egypt?",
+        options: ["Amazon", "Nile", "Una"],
+        answer: "Nile"
+    },
+    {
+        question: "Which country belong to the Scandinavian countries?",
+        options: ["England", "Sweden", "Montenegro", "Germany", "Poland"],
+        answer: "Sweden"
+    },
+    {
+        question: "Where is Disneyland in Europa?",
+        options: ["Munich", "Paris", "Vienna"],
+        answer: "Paris"
+    },
+    {
+        question: "How many continents are there in the World?",
+        options: ["5", "7", "6"],
+        answer: "7"
+    },
+    {
+        question: "What is the smallest country in the World?",
+        options: ["Monaco", "Vatican City", "Nauru"],
+        answer: "Vatican City"
+    },
+    {
+        question: "What is the largest desert in the world?",
+        options: ["Sahara", "Antartica", "Gobi"],
+        answer: "Antartica"
+    },
 ];
 
 let currentQuestionIndex = 0;
 let correctAnswers = 0;
-let incorrectAnswers = 0; 
+let incorrectAnswers = 0;
 
 const questionText = document.getElementById("question-text");
 const answerOptions = document.getElementById("answer-options");
@@ -110,8 +110,8 @@ function checkAnswer(selectedOption) {
 }
 
 function updateScore() {
-        scoreDisplay.textContent = `Correct: ${correctAnswers} | Incorrect: ${incorrectAnswers}`;
-    }
+    scoreDisplay.textContent = `Correct: ${correctAnswers} | Incorrect: ${incorrectAnswers}`;
+}
 
 function endQuiz() {}
 
